adac4c29c3b8022b78eba38291bf658ba49f7895
KIM MINJI <sml07113@gmail.com>
2022-12-03 17:46:44 +0900

    [Nest] Board Module, Controller, Serice 생성

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -1,10 +1,7 @@
+import { BoardsModule } from './boards/boards.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule],
 })
 export class AppModule {}
