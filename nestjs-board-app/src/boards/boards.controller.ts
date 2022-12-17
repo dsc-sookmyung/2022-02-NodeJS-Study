fc9d875e59859cb2b2edaca63415bbf2f68ec74d
KIM MINJI <sml07113@gmail.com>
2022-12-17 22:57:01 +0900

    [Nest] 게시물 Create

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Body, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -9,5 +9,13 @@ export class BoardsController {
   @Get()
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
