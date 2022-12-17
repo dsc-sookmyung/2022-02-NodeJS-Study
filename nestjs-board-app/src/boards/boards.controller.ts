589a44cc3c8a9a1d3edbfed109e067d8b67f5132
KIM MINJI <sml07113@gmail.com>
2022-12-17 22:08:48 +0900

    [Nest] 모든 게시물 Read / Model 정의

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
 }
--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; // 다른 컴포넌트에서 변경하는 것 차단
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+}
